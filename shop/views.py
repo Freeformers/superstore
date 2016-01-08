64e5c29e123298abde2e09dc24ee25fc3093e551
Shaun Stanworth <shaun@freeformers.com>
2016-01-08 15:25:31 +0000

    Added product range

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Department, Product, Review
 
@@ -27,3 +28,6 @@ class CreateProduct(CreateView):
     model = Product
     fields = ['name', 'description', 'image_url', 'department', 'colour']
     success_url = '/'
+    
+    def get_success_url(self):
+        return reverse_lazy('shop-department', kwargs={'pk': self.object.department_id})
